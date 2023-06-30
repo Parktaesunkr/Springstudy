f49e4b2d2db1c97cc092575b37ce241bb8c001ca
Parktaesunkr <spcsm5452@naver.com>
2023-06-30 17:31:42 +0900

    게시판
    페이징, 검색기능 추가(css는 미첨가)

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,90 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	private BoardService service;
+	
+	/*@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}*/
+	
+	@GetMapping("/list")
+	public void list(Criteria cri, Model model) {
+		log.info("list : " + cri);
+		model.addAttribute("list", service.getList(cri));
+		//model.addAttribute("pageMaker", new PageDTO(cri, 234)); // PageDTO(cri, 123)의 순서는 PageDTO의 생성자와 같아야 한다.
+		
+		int total = service.getTotal(cri);
+		
+		log.info("total : "+ total);
+		
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: "+ board);
+		
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/register") // get은 받은게 없으면 출력하지 않음
+	public void register() {
+		
+	}
+	
+	@GetMapping({"/get", "/modify"})
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+		log.info("/get or modify");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board,Criteria cri, RedirectAttributes rttr) {
+		log.info("modify : "+board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list"+cri.getListLink();
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr) {
+	log.info("remove....."+bno);
+		
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list" + cri.getListLink();
+		
+	}
+	
+}
